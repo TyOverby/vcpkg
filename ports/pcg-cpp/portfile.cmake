0a4c2e136914ea6c87d44682d9c5118238e09aaf
Alexander Neumann <Alexander.Neumann@hamburg.de>
2017-09-30 22:40:36 +0200

    finished pcg portfile.

--- ports/pcg-cpp/portfile.cmake
+++ ports/pcg-cpp/portfile.cmake
@@ -3,19 +3,29 @@ include(vcpkg_common_functions)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO imneme/pcg-cpp
-    REF 0ca2e8ea6ba212bdfbc6219c2313c45917e34b8d
-    SHA512 0d15d1336950b19e796fa3cbcd69396a487ff20efef5b2d409001a067e5328031c0555bc85289977dd4ab84de7c5fc5ec436954f5d23e03733893f9a37f6ba2e
-    HEAD_REF master
+    REPO Neumann-A/pcg-cpp
+    REF 93b77ae39315699bf532a949142bac3be01e2dc9
+    SHA512 1700ef6a4498bf526e06647872cab8de60179e3fed398d22551ff197fa66a076208a4aa84bf777877fd4689fa38b9f42a03f40f14535cbfc12d7f6c99c5e09fe
+    HEAD_REF vs_2017fix_and_cmake
 )
-#vcpkg_extract_source_archive(${ARCHIVE})
+
+VCPKG_CONFIGURE_CMAKE(
+    SOURCE_PATH ${SOURCE_PATH}
+	PREFER_NINJA
+    OPTIONS
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH "cmake")
 
 # Put the licence file where vcpkg expects it
 file(COPY ${SOURCE_PATH}/license-mit.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/pcg-cpp)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/pcg-cpp/license-mit.txt ${CURRENT_PACKAGES_DIR}/share/pcg-cpp/copyright)
 
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
-# Copy the rapidjson header files
+# Copy the pcg-cpp header files
 file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR} FILES_MATCHING PATTERN "*.hpp")
 vcpkg_copy_pdbs()
 
