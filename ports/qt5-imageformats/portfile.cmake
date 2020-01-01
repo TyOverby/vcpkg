87fca39daed297ce594706d55840aafe419e4663
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-01-01 22:42:51 +0100

    update qt5-imageformat according to the new requirements for 5.14

--- ports/qt5-imageformats/portfile.cmake
+++ ports/qt5-imageformats/portfile.cmake
@@ -21,6 +21,8 @@ find_library(WEBP_RELEASE NAMES webp PATHS "${CURRENT_INSTALLED_DIR}/lib" NO_DEF
 find_library(WEBP_DEBUG NAMES webpd webp PATHS "${CURRENT_INSTALLED_DIR}/debug/lib" NO_DEFAULT_PATH)
 find_library(WEBPDEMUX_RELEASE NAMES webpdemux PATHS "${CURRENT_INSTALLED_DIR}/lib" NO_DEFAULT_PATH) 
 find_library(WEBPDEMUX_DEBUG NAMES webpdemuxd webpdemux PATHS "${CURRENT_INSTALLED_DIR}/debug/lib" NO_DEFAULT_PATH)
+find_library(WEBPMUX_RELEASE NAMES webpmux libwebpmux PATHS "${CURRENT_INSTALLED_DIR}/lib" NO_DEFAULT_PATH) 
+find_library(WEBPMUX_DEBUG NAMES webpmuxd webpmux libwebpmuxd libwebpmux PATHS "${CURRENT_INSTALLED_DIR}/debug/lib" NO_DEFAULT_PATH)
 # Depends on opengl in default build but might depend on giflib, libjpeg-turbo, zlib, libpng, tiff, freeglut (!osx), sdl1 (windows) 
 # which would require extra libraries to be linked e.g. giflib freeglut sdl1 other ones are already linked
 
@@ -29,9 +31,11 @@ find_library(LZMA_RELEASE lzma PATHS "${CURRENT_INSTALLED_DIR}/lib" NO_DEFAULT_P
 find_library(LZMA_DEBUG lzmad lzma PATHS "${CURRENT_INSTALLED_DIR}/debug/lib" NO_DEFAULT_PATH)
 
 set(OPT_REL "TIFF_LIBS=${TIFF_RELEASE} ${LZMA_RELEASE}"
-            "WEBP_LIBS=${WEBP_RELEASE} ${WEBPDEMUX_RELEASE}" 
+            "WEBP_LIBS=${WEBP_RELEASE} ${WEBPDEMUX_RELEASE} ${WEBPMUX_RELEASE}" 
             "JASPER_LIBS=${JASPER_RELEASE} ${FREEGLUT_RELEASE}") # This will still fail if LIBWEBP is installed with all available features due to the missing additional dependencies
 set(OPT_DBG "TIFF_LIBS=${TIFF_DEBUG} ${LZMA_DEBUG}"
-            "WEBP_LIBS=${WEBP_DEBUG} ${WEBPDEMUX_DEBUG}"
+            "WEBP_LIBS=${WEBP_DEBUG} ${WEBPDEMUX_DEBUG} ${WEBPMUX_DEBUG}"
             "JASPER_LIBS=${JASPER_DEBUG} ${FREEGLUT_DEBUG}")
+list(APPEND CORE_OPTIONS "WEBP_INCDIR=${CURRENT_INSTALLED_DIR}/include") # Requires libwebp[all]
+
 qt_submodule_installation(BUILD_OPTIONS ${CORE_OPTIONS} BUILD_OPTIONS_RELEASE ${OPT_REL} BUILD_OPTIONS_DEBUG ${OPT_DBG})
