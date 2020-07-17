3fb5be3b8615c83ce768fa1d10a149c999821903
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-07-17 22:03:14 +0200

    fix a few iconv deps

--- ports/exiv2/vcpkg-cmake-wrapper.cmake
+++ ports/exiv2/vcpkg-cmake-wrapper.cmake
@@ -1,11 +1,11 @@
 _find_package(${ARGS})
 
 if("@VCPKG_LIBRARY_LINKAGE@" STREQUAL "static")
-    find_package(unofficial-iconv CONFIG REQUIRED)
+    find_package(Iconv REQUIRED)
     find_package(unofficial-gettext CONFIG REQUIRED)
     if(TARGET exiv2lib)
         set_property(TARGET exiv2lib APPEND PROPERTY INTERFACE_LINK_LIBRARIES 
-            unofficial::iconv::libiconv 
+            Iconv::Iconv
             unofficial::gettext::libintl)
     endif()
 endif()
